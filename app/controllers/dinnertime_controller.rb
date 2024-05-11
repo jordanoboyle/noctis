99f4478e0a2f8d29c9bd3c7b13edd2890dc8ef85
jordanoboyle <jmcmcode@gmail.com>
2024-05-10 21:28:49 -0600

    Added blizzard and electric routes to dinnertime controller

--- app/controllers/dinnertime_controller.rb
+++ app/controllers/dinnertime_controller.rb
@@ -1,2 +1,17 @@
 class DinnertimeController < ApplicationController
+
+  def thunder
+    render json: {
+      electric_mag1: "Thunder",
+      electric_mag2: "Thundera",
+      electric_mag3: "Thunderga"
+    }
+  end
+
+  def blizzard
+    render json: {
+      ice_mag1: "blizzard",
+      ice_mag2: "blizzara"
+    }
+  end
 end
